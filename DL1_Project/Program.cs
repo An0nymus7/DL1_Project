1db663037e1ae66efe4bc37de7a984e28bb243e0
An0nymus7 <horvath.levi077@gmail.com>
2024-10-18 01:09:31 +0200

    Add project files.

---
+++ DL1_Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DL1_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
